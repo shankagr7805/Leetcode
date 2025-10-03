9e78a1169594a771b7659271d9f5e1cf91f9300c
Shankar Agrawal <iamshank7805@gmail.com>
2025-10-03 20:11:26 +0530

    Time: 48 ms (72.00%) | Memory: 55 MB (87.82%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -5,17 +5,14 @@ class Solution {
 
         while (r < fruits.length) {
             mp.put(fruits[r], mp.getOrDefault(fruits[r], 0) + 1);
-            if (mp.size() <= 2) {
-                mx = Math.max(mx, r - l + 1);
-            } else {
-                while(mp.size() > 2) {
-                    mp.put(fruits[l], mp.get(fruits[l]) - 1);
-                    if (mp.get(fruits[l]) == 0) {
-                        mp.remove(fruits[l]);
-                    }
-                    l++;
-                } 
+            while (mp.size() > 2) {
+                mp.put(fruits[l], mp.get(fruits[l]) - 1);
+                if (mp.get(fruits[l]) == 0) {
+                    mp.remove(fruits[l]);
+                }
+                l++;
             }
+            mx = Math.max(mx, r - l + 1);
             r++;
         }
         return mx;
