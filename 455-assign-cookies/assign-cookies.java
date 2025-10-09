75912494b063e44103626ac07562511bf54d15ab
Shankar Agrawal <iamshank7805@gmail.com>
2025-10-10 01:52:42 +0530

    Time: 9 ms (98.98%) | Memory: 48.1 MB (96.97%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -2,15 +2,18 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
+
         int l = 0;
-        int r = 0; 
-        while(r < s.length) {
+        int r = 0;
+
+        while(r < s.length){
             if(l>=g.length) break;
-            if(s[r] >= g[l]) {
+            if(s[r] >= g[l]){
                 l++;
-            } 
+            }
             r++;
         }
+
         return l;
     }
 }
