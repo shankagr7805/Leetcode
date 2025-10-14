fe49665761b53e13e41b0b170d709f14da6c6e79
Shankar Agrawal <iamshank7805@gmail.com>
2025-10-14 19:54:51 +0530

    Time: 9 ms (7.02%) | Memory: 41.6 MB (5.27%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,10 +1,13 @@
 class Solution {
     public int reverseBits(int n) {
+        String str = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0');
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        String s = sb.toString();
+
         int ans = 0;
-        for (int i = 0; i < 32; i++) {
-            ans <<= 1;        
-            ans |= (n & 1);   
-            n >>= 1;          
+        for (int i = 0; i < s.length(); i++) {
+            ans = ans * 2 + (s.charAt(i) - '0');
         }
         return ans;
     }
