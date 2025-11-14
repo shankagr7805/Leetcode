99f2cdfed6840a83b74618561f36b4e81bdbec97
Shankar Agrawal <iamshank7805@gmail.com>
2025-11-14 18:50:06 +0530

    Time: 384 ms (26.99%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,4 +1,5 @@
-SELECT email AS Email
-FROM Person
-GROUP BY email
-HAVING COUNT(*) > 1;
+SELECT DISTINCT P1.email AS Email
+FROM Person P1
+JOIN Person P2
+    ON P1.email = P2.email
+   AND P1.id <> P2.id;
