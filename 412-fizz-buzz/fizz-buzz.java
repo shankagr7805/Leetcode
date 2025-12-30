d590cb3775a620e91208c81fd93794b1eae2c002
Shankar Agrawal <iamshank7805@gmail.com>
2025-12-31 00:52:39 +0530

    Time: 1 ms (99.85%) | Memory: 46.7 MB (64.74%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -6,7 +6,7 @@ class Solution {
             if(i%3==0 && i%5==0) ans.add("FizzBuzz");
             else if(i%3==0) ans.add("Fizz"); 
             else if(i%5==0) ans.add("Buzz");
-            else ans.add(""+i);
+            else ans.add(String.valueOf(i));
 
             i++;
         }
