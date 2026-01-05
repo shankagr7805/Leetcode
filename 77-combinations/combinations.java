c4d4c597691be493138ec43018e66a214b5832a9
Shankar Agrawal <iamshank7805@gmail.com>
2026-01-06 01:23:08 +0530

    Time: 18 ms (80.72%) | Memory: 98.4 MB (6.86%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,27 +1,24 @@
-import java.util.*;
-
 class Solution {
-    public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(1, n, k, new ArrayList<>(), result);
-        return result;
-    }
+    private List<List<Integer>> ans = new ArrayList<>();
+    private int k;
+    private List<Integer> t = new ArrayList<>();
 
-    private void backtrack(int start, int n, int k,
-                           List<Integer> temp,
-                           List<List<Integer>> result) {
-
-        // Base case: k elements chosen
-        if (temp.size() == k) {
-            result.add(new ArrayList<>(temp));
+    private void dfs(int j, int n) {
+        if (t.size() == k) {
+            ans.add(new ArrayList<>(t));
             return;
         }
-
-        // Choose next numbers
-        for (int i = start; i <= n; i++) {
-            temp.add(i);                 // choose
-            backtrack(i + 1, n, k, temp, result); // explore
-            temp.remove(temp.size() - 1); // un-choose (backtrack)
+        for (int i = j; i <= n; i++) {
+            t.add(i);
+            dfs(i + 1, n);
+            t.remove(t.size() - 1);
         }
+
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        this.k = k;
+        
+        dfs(1, n);
+        return ans;
     }
 }
