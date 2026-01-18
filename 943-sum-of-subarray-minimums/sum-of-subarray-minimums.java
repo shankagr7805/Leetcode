9f4c735d06e243c4f8f2d0a5b00349a38eeacb1f
Shankar Agrawal <iamshank7805@gmail.com>
2026-01-19 00:14:35 +0530

    Time: 71 ms (35.07%) | Memory: 57.8 MB (43.67%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,41 +1,42 @@
-import java.util.*;
-
 class Solution {
-    public int sumSubarrayMins(int[] arr) {
-        int n = arr.length;
-        int MOD = 1_000_000_007;
-
-        int[] left = new int[n];
-        int[] right = new int[n];
-
+    private int[] findNSE(int[] arr) {
+        int[] nse = new int[arr.length];
         Stack<Integer> st = new Stack<>();
 
-        // Previous Smaller Element (strictly smaller)
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+        for(int i=arr.length-1; i>=0; i--) {
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]) {
                 st.pop();
             }
-            left[i] = st.isEmpty() ? (i + 1) : (i - st.peek());
+            nse[i] = (st.isEmpty()) ? arr.length : st.peek(); 
             st.push(i);
         }
+        return nse;
+    }
+    private int[] findPSE(int[] arr) {
+        int[] pse = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
 
-        st.clear();
-
-        // Next smaller element.
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+        for(int i=0; i<arr.length; i++) {
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]) {
                 st.pop();
             }
-            right[i] = st.isEmpty() ? (n - i) : (st.peek() - i);
+            pse[i] = (st.isEmpty()) ? -1 : st.peek(); 
             st.push(i);
         }
+        return pse;
+    }
+    public int sumSubarrayMins(int[] arr) {
+        int ans = 0;
+        int MOD = 1000000007;
+        int[] nse = findNSE(arr);
+        int[] pse = findPSE(arr);
 
-        long ans = 0;
-        for (int i = 0; i < n; i++) {
-            long contrib = (long) arr[i] * left[i] * right[i];
-            ans = (ans + contrib) % MOD;
+        for(int i=0; i<arr.length; i++) {
+            long left = i - pse[i] ;
+            long right = nse[i] - i;
+            long cal = (left * right * arr[i]) % MOD ;
+            ans = (ans + (int) cal) % MOD;
         }
-
-        return (int) ans;
+        return ans;
     }
 }
