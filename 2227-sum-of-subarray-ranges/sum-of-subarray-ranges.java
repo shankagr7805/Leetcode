cdb7900090aaa64cebd9d8a25cee4c859a4318b2
Shankar Agrawal <iamshank7805@gmail.com>
2026-01-19 14:38:31 +0530

    Time: 13 ms (97.12%) | Memory: 46.5 MB (55.19%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,69 +1,22 @@
-import java.util.*;
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        int n = nums.length;
-
-        long sumMax = 0;
-        long sumMin = 0;
-
-        // -------- SUM OF SUBARRAY MINIMUMS --------
-        Stack<Integer> st = new Stack<>();
-        int[] leftMin = new int[n];
-        int[] rightMin = new int[n];
-
-        // Previous smaller (strictly smaller)
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && nums[st.peek()] > nums[i]) {
-                st.pop();
-            }
-            leftMin[i] = st.isEmpty() ? (i + 1) : (i - st.peek());
-            st.push(i);
-        }
-
-        st.clear();
-
-        // Next smaller or equal
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && nums[st.peek()] >= nums[i]) {
-                st.pop();
-            }
-            rightMin[i] = st.isEmpty() ? (n - i) : (st.peek() - i);
-            st.push(i);
-        }
-
-        for (int i = 0; i < n; i++) {
-            sumMin += (long) nums[i] * leftMin[i] * rightMin[i];
-        }
-
-        // -------- SUM OF SUBARRAY MAXIMUMS --------
-        st.clear();
-        int[] leftMax = new int[n];
-        int[] rightMax = new int[n];
-
-        // Previous greater (strictly greater)
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && nums[st.peek()] < nums[i]) {
-                st.pop();
-            }
-            leftMax[i] = st.isEmpty() ? (i + 1) : (i - st.peek());
-            st.push(i);
-        }
-
-        st.clear();
-
-        // Next greater or equal
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && nums[st.peek()] <= nums[i]) {
-                st.pop();
-            }
-            rightMax[i] = st.isEmpty() ? (n - i) : (st.peek() - i);
-            st.push(i);
-        }
-
-        for (int i = 0; i < n; i++) {
-            sumMax += (long) nums[i] * leftMax[i] * rightMax[i];
-        }
-
-        return sumMax - sumMin;
+    public long subArrayRanges(int[] arr) {
+        long sum = 0; int n = arr.length;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0; i <= n; i++){
+            while(!stack.isEmpty() && (i == n || arr[stack.peek()] >= arr[i])){
+                int el = stack.pop();
+                int pse = (stack.isEmpty())? -1 : stack.peek();
+                int nse = i;
+                sum -= (long)arr[el] * (el - pse) * (nse - el);
+            }stack.push(i);
+        }stack.clear();
+        for(int i = 0; i <= n; i++){
+            while(!stack.isEmpty() && (i == n || arr[stack.peek()] < arr[i])){
+                int el = stack.pop();
+                int pse = (stack.isEmpty())? -1 : stack.peek();
+                int nse = i;
+                sum += (long)arr[el] * (el - pse) * (nse - el);
+            }stack.push(i);
+        }return sum;
     }
 }
