7dba8170fd7ced5fd3bad26d29f76c1f56fed6ee
Shankar Agrawal <iamshank7805@gmail.com>
2026-01-20 22:03:22 +0530

    Time: 6 ms (88.38%) | Memory: 49.1 MB (82.05%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,45 @@
+class Solution {
+    private int largestArea(int[] arr) {
+        Deque<Integer> st = new ArrayDeque<>();
+        int maxArea = 0; int n = arr.length;
+
+        for(int i=0; i<n; i++) {
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                int el = st.peek();
+                st.pop();
+                int nse = i;
+                int pse = (st.isEmpty()) ? -1 : st.peek();
+
+                maxArea = Math.max(maxArea, arr[el] * (nse-pse-1));
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()) {
+            int el = st.peek();
+            st.pop();
+            int nse = n;
+            int pse = (st.isEmpty()) ? -1 : st.peek();
+
+            maxArea = Math.max(maxArea, arr[el] * (nse-pse-1));
+        }
+        return maxArea;
+    }
+    public int maximalRectangle(char[][] matrix) {
+        int m = matrix.length; int n = matrix[0].length;
+        int[][] preMatrix = new int[m][n];
+        for(int i=0; i<n; i++) {
+            int preSum = 0;
+            for(int j=0; j<m; j++) {  
+                if(matrix[j][i] == '1') preSum++;
+                else preSum = 0;
+                preMatrix[j][i] = preSum;
+            }
+        }
+        int ans = 0;
+        for(int k=0; k<m; k++) {
+            ans = Math.max(ans , largestArea(preMatrix[k]));
+        }
+
+        return ans;
+    }
+}
