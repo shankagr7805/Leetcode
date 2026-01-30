4892a1c378a4b106347e8c11a01850a22a38a209
Shankar Agrawal <iamshank7805@gmail.com>
2026-01-30 19:55:28 +0530

    Time: 45 ms (62.80%) | Memory: 43.8 MB (92.82%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,29 +1,22 @@
-class Solution {
-    private boolean subsetSum(int[] nums, int k) {
-        int n = nums.length;
-        boolean[] prev = new boolean[k+1];
-        prev[0] = true;
-        if(nums[0] <= k) prev[nums[0]] = true;
-
-        for(int i=1; i<n; i++) {
-            boolean[] curr = new boolean[k+1];
-            curr[0] = true;
-            for(int j=1; j<=k; j++) {
-                boolean notTake = prev[j];
-                boolean take = false;
-                if(nums[i] <= j) take = prev[j-nums[i]];
-                curr[j] = take || notTake ;
-            }
-            prev = curr;
-        }
-        return prev[k];
-    } 
+class Solution { 
     public boolean canPartition(int[] nums) {
         int n = nums.length; int sum = 0;
         for(int i=0; i<n; i++) {
             sum += nums[i];
         }
         if(sum % 2 != 0) return false;
-        return subsetSum(nums, sum/2);
+        sum = sum/2;
+        
+        boolean[] dp = new boolean[sum+1];
+        dp[0] = true;
+
+        for(int i=1; i<n; i++) {
+            for(int j=sum; j>0; j--) {
+                if (j - nums[i - 1] < 0) dp[j] = dp[j];
+                else dp[j] = dp[j-nums[i-1]] || dp[j];
+            }
+        }
+
+        return dp[sum];
     }
 }
