850a6119e668ea440daa30ef3f2e66ee4f6bcfe2
Shankar Agrawal <iamshank7805@gmail.com>
2026-02-01 19:24:17 +0530

    Time: 14 ms (92.83%) | Memory: 51.6 MB (49.83%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,19 +1,19 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        int i = 0; int j=0;
-        int m = g.length; int n = s.length;
         Arrays.sort(g);
         Arrays.sort(s);
-        int cnt = 0;
-        while(i != m && j != n) {
-            if(g[i] <= s[j]) {
-                i++;
-                j++;
-                cnt++;
-            } else {
-                j++;
+
+        int l = 0;
+        int r = 0;
+
+        while(r < s.length){
+            if(l>=g.length) break;
+            if(s[r] >= g[l]){
+                l++;
             }
+            r++;
         }
-        return cnt;
+
+        return l;
     }
 }
