c7d3f1171b6e67a3ba9904339d61079cfa152890
Shankar Agrawal <iamshank7805@gmail.com>
2026-02-03 21:58:03 +0530

    Time: 6 ms (78.99%) | Memory: 43.2 MB (71.60%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,27 +2,14 @@ class Solution {
     public int change(int k, int[] coins) {
         int n = coins.length;
         int[] prev = new int[k+1];
-        for(int j=0; j<k+1; j++) {
-            if(j % coins[0] == 0) {
-                prev[j] = 1;
-            } else {
-                prev[j] = 0;
+    
+        prev[0] = 1;
+
+        for(int i=0; i<n; i++) {
+            for(int j=coins[i]; j<=k; j++) {
+                prev[j] += prev[j-coins[i]];
             }
         }
-
-        for(int i=1; i<n; i++) {
-            int[] curr = new int[k+1];
-            for(int j=0; j<=k; j++) {
-                int notTake = prev[j];
-                int take = 0;
-                if(coins[i] <= j) {
-                    take = curr[j-coins[i]];
-                }
-                curr[j] = take + notTake;
-            }
-            prev = curr;
-        }
-
         return prev[k];
     }
 }
