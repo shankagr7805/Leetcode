2d93ca91de616d3b2340af8735824fead4536a8e
Shankar Agrawal <iamshank7805@gmail.com>
2026-02-17 00:21:46 +0530

    Time: 5 ms (65.96%) | Memory: 47 MB (83.54%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -8,7 +8,6 @@ class Solution {
 
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
-
                 if(str1.charAt(i-1) == str2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1];
                 } else {
