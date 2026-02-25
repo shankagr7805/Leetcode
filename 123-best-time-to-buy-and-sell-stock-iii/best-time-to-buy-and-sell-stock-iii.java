46af8c98f403a8806d0c1af1d0fec4a0ec687e52
Shankar Agrawal <iamshank7805@gmail.com>
2026-02-25 20:09:46 +0530

    Time: 55 ms (36.87%) | Memory: 117.8 MB (37.97%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,25 +1,26 @@
 class Solution {
-    private int f(int idx, int buy, int k, int[] prices, int[][][] dp) {
-        if(idx == prices.length) return 0;
-        int p = 0;
-        if(k == 0) return 0;
-        if(dp[idx][buy][k] != -1) return dp[idx][buy][k];
-        if(buy == 1) {
-            p = Math.max(-prices[idx] + f(idx+1, 0, k, prices, dp), f(idx+1, 1, k, prices, dp));
-        } else {
-            p = Math.max(prices[idx] + f(idx+1, 1, k-1, prices, dp), f(idx+1, 0, k, prices, dp));
-        }
-        return dp[idx][buy][k] = p;
-    }
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][3];
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<3; j++) {
-                dp[i][0][j] = -1;
-                dp[i][1][j] = -1;
+        int[][][] dp = new int[n+1][2][3];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<2; j++) {
+                dp[i][j][0] = 0;
             }
         }
-        return f(0, 1, 2, prices, dp);
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<2; j++) {
+                for(int k=1; k<3; k++) {
+                    // Buy
+                    if(j == 1) {            
+                        dp[i][j][k] = Math.max(-prices[i] + dp[i+1][0][k] , dp[i+1][1][k]);
+                    } else {                
+                        //Cannot Buy
+                        dp[i][j][k] = Math.max(prices[i] + dp[i+1][1][k-1] , dp[i+1][0][k]);
+                    }
+                }
+            }
+        }
+        
+        return dp[0][1][2];
     }
 }
