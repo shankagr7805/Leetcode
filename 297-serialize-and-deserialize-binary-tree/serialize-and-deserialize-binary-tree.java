03cfbb4ec3d15fdee8b9dd285fa53dcd55365642
Shankar Agrawal <iamshank7805@gmail.com>
2026-06-07 00:01:32 +0530

    Time: 9 ms (85.72%) | Memory: 49 MB (8.64%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -11,48 +11,39 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if(root == null) return "";
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
         StringBuilder sb = new StringBuilder();
-        q.add(root);
-        while(!q.isEmpty()) {
-            TreeNode node = q.poll();
-            if(node == null) {
-                sb.append("n ");
-                continue;
-            }
-            sb.append(node.val + " ");
-            q.add(node.left);
-            q.add(node.right);
-        } 
+        serial(root, sb);
         return sb.toString();
+    }
+
+    private void serial(TreeNode root, StringBuilder sb){
+        if(root == null){
+            sb.append("#,");
+            return;
+        }
+
+        sb.append(root.val).append(',');
+
+        serial(root.left, sb);
+        serial(root.right, sb);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if(data == "") return null;
-        Queue<TreeNode> q = new LinkedList<>();
-        String[] values = data.split(" ");
-        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
-        q.add(root);
-        for(int i=1; i<values.length; i++) {
-            TreeNode parent = q.poll();
-            if(!values[i].equals("n")) {
-                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
-                parent.left = left;
-                q.add(left);
-            } 
-            if(!values[++i].equals("n")) {
-                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
-                parent.right = right;
-                q.add(right);
-            }
-        }
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return deserial(queue);
+         
+    }
+
+    private TreeNode deserial(Queue<String> queue){
+        if(queue.isEmpty()) return null;
+
+        String val = queue.poll();
+        if(val.equals("#")) return null;
+
+        TreeNode root = new TreeNode(Integer.valueOf(val));
+        root.left = deserial(queue);
+        root.right = deserial(queue);
         return root;
     }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec ser = new Codec();
-// Codec deser = new Codec();
-// TreeNode ans = deser.deserialize(ser.serialize(root));
