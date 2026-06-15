7dc66699bbef74151b25c2f9983b45b3fd834796
Shankar Agrawal <iamshank7805@gmail.com>
2026-06-15 13:58:35 +0530

    Time: 1 ms (38.05%) | Memory: 46.5 MB (89.01%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,24 @@
+class Solution {
+    private void dfs(int r, int c, int[][] ans, int[][] image, int color, int[] dr, int[] dc, int in) {
+        ans[r][c] = color;
+        int n = image.length;
+        int m = image[0].length;
+        for(int i=0; i<4; i++) {
+            int nrow = r + dr[i];
+            int ncol = c + dc[i];
+
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol] == in && ans[nrow][ncol] != color) {
+                dfs(nrow, ncol, ans, image, color, dr, dc, in);
+            }
+        }
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(color == image[sr][sc]) return image;
+        int in = image[sr][sc];
+        int[][] ans = image;
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
+        dfs(sr, sc, ans, image, color, dr, dc, in);
+        return ans;
+    }
+}
