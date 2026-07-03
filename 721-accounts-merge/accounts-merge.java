1a14501ac498c823dac13ee30e66b4156ad7151b
Shankar Agrawal <iamshank7805@gmail.com>
2026-07-03 14:31:33 +0530

    Time: 28 ms (90.41%) | Memory: 49.5 MB (67.88%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,10 +1,8 @@
 class DisjointSet {
-    List<Integer> rank = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
     List<Integer> size = new ArrayList<>();
     public DisjointSet(int n) {
         for(int i=0; i<=n; i++) {
-            rank.add(0);
             parent.add(i);
             size.add(1);
         }
@@ -14,21 +12,6 @@ class DisjointSet {
         int ulp = findUlP(parent.get(node));
         parent.set(node, ulp);
         return parent.get(node);
-    }
-    public void unionByRank(int u, int v) {
-        int ulp_u = findUlP(u);
-        int ulp_v = findUlP(v);
-        if(ulp_u == ulp_v) return;
-
-        if(rank.get(ulp_u) < rank.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-        } else if(rank.get(ulp_v) < rank.get(ulp_u)) {
-            parent.set(ulp_v, ulp_u);
-        } else {
-            parent.set(ulp_v, ulp_u);
-            int rankU = rank.get(ulp_u);
-            rank.set(ulp_u, rankU+1);
-        }
     }
     public void unionBySize(int u, int v) {
         int ulp_u = findUlP(u);
