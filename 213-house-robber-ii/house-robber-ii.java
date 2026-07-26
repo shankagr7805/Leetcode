574c156cf65fdad6e9b5df10d9471c604d0a7a54
Shankar Agrawal <iamshank7805@gmail.com>
2026-07-26 08:42:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (42.16%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,24 +1,16 @@
 class Solution {
-    public int rob_adj(int[] arr) {
-        int n = arr.length;
-        int prev = arr[0];
-        int prev2 = 0;
+    private int f(int[] nums, int i, int[] dp) {
+        if(i==0) return nums[0];
+        if(i<0) return 0;
+        if(dp[i] != -1) return dp[i];
+        int take = f(nums, i-2, dp) + nums[i];
+        int not = f(nums, i-1, dp);
 
-        for(int i=1; i<n; i++) {
-            int take = arr[i];
-            if(i>1) take += prev2;
-
-            int notTake = prev;
-            int curr = Math.max(take, notTake);
-            prev2 = prev;
-            prev = curr;
-        }
-
-        return prev;
+        return dp[i] = Math.max(take, not);
     }
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n==1) return nums[0];
+        if (n == 1) return nums[0];
         int[] temp1 = new int[n-1];
         int[] temp2 = new int[n-1];
         for(int i=0; i<n-1; i++) {
@@ -27,7 +19,12 @@ class Solution {
         for(int i=1; i<n; i++) {
             temp2[i-1] = nums[i];
         }
+        int[] dp = new int[n-1];
+        Arrays.fill(dp, -1);
+        int a = f(temp1, n-2, dp);
+        Arrays.fill(dp, -1);
+        int b = f(temp2, n-2, dp);
 
-        return Math.max(rob_adj(temp1) , rob_adj(temp2));
+        return Math.max(a, b);
     }
-}
+} 
