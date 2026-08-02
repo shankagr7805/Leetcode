59458e1165e03f8488db370e672d07b8f805a0ad
Shankar Agrawal <iamshank7805@gmail.com>
2026-08-02 10:07:37 +0530

    Time: 17 ms (81.42%) | Memory: 54.7 MB (24.54%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int numDistinct(String s, String t) {
-        int n1 = s.length();
-        int n2 = t.length();
-        int[] prev = new int[n2+1];
-        prev[0] = 1;
-        for(int i=1; i<=n1; i++) {
-            int[] curr = new int[n2+1];
-            curr[0] = 1;
-            for(int j=1; j<=n2; j++) {
-                if(s.charAt(i-1) == t.charAt(j-1)) curr[j] = prev[j-1] + prev[j]; 
-                else curr[j] = prev[j];
-            }
-            prev = curr;
+    private int f(String s, String t, int i, int j, int[][] dp) {
+        if(j<0) return 1;
+        if(i<0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s.charAt(i) == t.charAt(j)) {
+            return dp[i][j] = f(s, t, i-1, j-1, dp) + f(s, t, i-1, j, dp);
+        } else {
+            return dp[i][j] = f(s, t, i-1, j, dp);
         }
-        return prev[n2];
+    }
+    public int numDistinct(String s, String t) {
+        int n = s.length(); int m = t.length();
+        int[][] dp = new int[n][m];
+        for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
+        return f(s, t, n-1, m-1, dp);
     }
 }
