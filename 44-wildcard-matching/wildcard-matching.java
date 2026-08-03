d30fc2dd420f0f3a4d61b2a98bd3ba51064d8fb4
Shankar Agrawal <iamshank7805@gmail.com>
2026-08-03 09:17:38 +0530

    Time: 18 ms (68.82%) | Memory: 63.6 MB (12.49%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,33 +1,29 @@
 class Solution {
-    private boolean f(String s, String p, int i, int j, int[][] dp) {
-        if(i<0 && j<0) return true;
-        if(j<0) return false;
+    private boolean f(String s, String t, int i, int j, int[][] dp) {
+        if(i < 0 && j < 0) return true;
+        if(j < 0) return false;
         if(i < 0) {
             for(int k = 0; k <= j; k++) {
-                if(p.charAt(k) != '*') return false;
+                if(t.charAt(k) != '*') return false;
             }
             return true;
         }
         if(dp[i][j] != -1) return dp[i][j] == 1;
-        boolean ans = false;
-        if(p.charAt(j) == '*') {
-            ans = f(s, p, i, j-1, dp) || f(s, p, i-1, j, dp);
+        boolean ans;
+        if(s.charAt(i) == t.charAt(j) || t.charAt(j) == '?') {
+            ans = f(s, t, i-1, j-1, dp);
+        } else if(t.charAt(j) == '*') {
+            ans = f(s, t, i-1, j, dp) || f(s, t, i, j-1, dp);
+        } else {
+            ans = false;
         }
-        if (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j)) ans = f(s, p, i-1, j-1, dp);
-        
         dp[i][j] = ans ? 1 : 0;
-
         return ans;
     }
     public boolean isMatch(String s, String p) {
-        int n = s.length();
-        int m = p.length();
+        int n = s.length(); int m = p.length();
         int[][] dp = new int[n][m];
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        return f(s, p, n-1, m-1, dp);
+        for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
+        return f(s, p, n-1, m-1,dp);
     }
 }
