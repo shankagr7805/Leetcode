4e733330b86d09fffae4c89cf2bae35d1a2cde7e
Shankar Agrawal <iamshank7805@gmail.com>
2026-08-09 12:54:00 +0530

    Time: 9 ms (10.97%) | Memory: 61.5 MB (90.30%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,24 +1,27 @@
 class Solution {
+    private int f(int[][] matrix, int i, int j, int[][] dp) {
+        if(matrix[i][j] == 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int l = 0; int u = 0; int dia = 0;
+        if(i>0) u = f(matrix, i-1, j, dp);
+        if(j>0) l = f(matrix, i, j-1, dp);
+        if(i>0 && j>0) dia = f(matrix, i-1, j-1, dp);
+
+        return dp[i][j] = Math.min(l, Math.min(u, dia)) + 1;
+    }
     public int countSquares(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
         int[][] dp = new int[n][m];
+        for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
+        int ans = 0;
+
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(matrix[i][j] == 0) continue;
-                int left = 0; int right = 0; int diag = 0;
-                if(i>0) left = dp[i-1][j];
-                if(j>0) right = dp[i][j-1];
-                if(i>0 && j>0) diag = dp[i-1][j-1];
-                dp[i][j] = Math.min(left, Math.min(right, diag)) + 1;
+                ans += f(matrix, i, j, dp);
             }
         }
-        int sum = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                sum += dp[i][j];
-            }
-        }
-        return sum;
+        return ans;
     }
 }
