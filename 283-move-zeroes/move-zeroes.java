48b34bd694fdae889ca79926b2e4279115bf8e92
Shankar Agrawal <iamshank7805@gmail.com>
2026-08-16 09:02:30 +0530

    Time: 1 ms (99.90%) | Memory: 47.6 MB (76.65%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int i=0;
+        for(int j=0; j<n; j++) {
+            if(nums[j] != 0) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        while(i<n) {
+            nums[i] = 0;
+            i++;
+        }
+    }
+}
